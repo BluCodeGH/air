a2512d2f8aa2d181abd886be9b7dc20cdb50c0db
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2021-01-07 18:16:32 -0500

    Implement template converter

--- converters/template.py
+++ converters/template.py
@@ -1,5 +1,6 @@
 import json
 from converter import Converter
+from converters import mcjson
 
 def update(d1, d2):
   for k in d2.keys():
@@ -26,31 +27,34 @@ class Template(Converter):
   def priority(self):
     return 5
 
-  # def dump(self, text):
-  #   data = json.loads(text)
-  #   try:
-  #     template = mcjson.dump(self._get("source/TEMPLATE.mcj"))
-  #   except FileNotFoundError:
-  #     #print("Not found.", text[:15])
-  #     return text
-  #   update(template, data)
-  #   return json.dumps(template, indent=2)
+  def dump(self, file):
+    if file.path.name == "TEMPLATE.json":
+      return None
+    if file.path.suffixes[-2:] != [".template", ".json"]:
+      return file
+    data = json.loads(file.contents)
+    #variables = data.pop("variables", {})
+    try:
+      template = mcjson.dump(file.source.with_name("TEMPLATE.mcj").read_text())
+    except FileNotFoundError:
+      print("Not found template for", file.source)
+      return file
+    update(template, data)
+    # template = updateVars(template, variables)
+    file.contents = json.dumps(template, indent=2)
+    return file
 
-  # def dump_path(self, path):
-  #   if path.name == "TEMPLATE.mcj":
-  #     raise ValueError
-  #   return path.suffixes[-2:] == [".template", ".mcj"]
-
-  # def load(self, text):
-  #   final = json.loads(text)
-  #   try:
-  #     template = mcjson.dump(self._get("source/TEMPLATE.mcj"))
-  #   except FileNotFoundError:
-  #     return text
-  #   original = difference(final, template)
-  #   return json.dumps(original)
-
-  # def load_path(self, path):
-  #   return path.suffixes[-2:] == [".template", ".json"]
+  def load(self, file):
+    if file.path.suffixes[-2:] != [".template", ".json"]:
+      return file
+    final = json.loads(file.contents)
+    try:
+      template = mcjson.dump(file.dest.with_name("TEMPLATE.mcj").read_text())
+    except FileNotFoundError:
+      print("Not found template for", file.dest)
+      return file
+    original = difference(final, template)
+    file.contents = json.dumps(original, indent=2)
+    return file
 
 Template()
